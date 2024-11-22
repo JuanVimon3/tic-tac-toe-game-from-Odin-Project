69a087c4af2afac5bc7bcbddf39310f29db0fe75
JuanVimon3 <jvillamonsalve@gmail.com>
2024-11-22 08:54:45 -0500

    WIP

---
+++ index.js
@@ -0,0 +1,9 @@
+function playGame (){
+  const inputName = (name) => {
+    name = alert(prompt('Please input your name'))
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+  playGame()
+})
