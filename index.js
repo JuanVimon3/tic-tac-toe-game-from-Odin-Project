d5bb81c82ace1f893c5ad2bf79e7a3362800a7ca
JuanVimon3 <jvillamonsalve@gmail.com>
2024-11-23 10:52:54 -0500

    wip

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-function playGame (){
-  const inputName = (name) => {
-    name = alert(prompt('Please input your name'))
-  }
-}
-
 document.addEventListener('DOMContentLoaded', function(){
-  playGame()
+  const cells = document.querySelectorAll('.cellsContainer div')
+  let currentPLayer = 'X'
+
+  cells.forEach((cell, index) => {
+    cell.addEventListener('click', function (){
+      if(cell.texContent === ''){
+        cell.texContent = currentPLayer;
+        chekWinner();
+        currentPLayer = currentPLayer === 'X' ? 'O' : 'X'
+      }
+    })
+  });
+
+  function chekWinner () {
+    const winnerPositions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    for (let possitions of winnerPositions) {
+      const [a, b ,c] = possitions;
+      if(cells[a].textContent === currentPLayer &&
+        cells[b].textContent === currentPLayer &&
+        cells[c].textContent === currentPLayer
+      ){
+        alert(`Player ${currentPLayer} wins`)
+        resetGame();
+        break
+      }
+    }
+  }
+
+  function resetGame () {
+    cells.forEach(cell => (cell.textContent = ''));
+    currentPLayer = 'X'
+  }
+  // function playGame (){
+  //   const winnerPossition = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],[0,4,8],[2,4,6]];
+    
+  //   const inputName = (name) => {
+  //     name = alert(prompt('Please input your name'))
+  //   }
+  //   const inputSymbol = (x, o) => {
+
+  //   } 
+  // }
 })
